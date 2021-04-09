6d64e6a974cc4d0174d27688308675747e83961a
bedolaszlobalazs <bedolaszlobalazs@gmail.com>
2021-04-09 10:09:23 +0200

    1.feladat

--- practice.py
+++ practice.py
@@ -1,2 +1,13 @@
-print("Hello world!")
+def max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
 
+
+def main():
+    print("Hello World!")
+    print(max(5, 6))
+
+if __name__ == "__main__":
+    main()
