92a203f7398e7c1a205831dc58c5e8f0c8d206bb
bedolaszlobalazs <bedolaszlobalazs@gmail.com>
2021-04-09 10:54:11 +0200

    1-4

--- practice.py
+++ practice.py
@@ -4,10 +4,39 @@ def max(a, b):
     else:
         return b
 
+def max2(a, b, c):
+    if a > b and a > c:
+        return a
+    elif b > c and b > a:
+        return b
+    else:
+        return c
+
+def length(list):
+    counter = 0
+    for x in list:
+        counter += 1
+    return counter
+
+def wovel(a):
+    vowel = ["a", "e", "i", "u", "o"]
+    for letter in vowel:
+        if letter == a:
+            return True
+    return False
+
 
 def main():
     print("Hello World!")
     print(max(5, 6))
+    print(max2(9, 8, 11))
+    kecske = [1, 3, 4, 5, 6, 8, 7, 5, 3]
+    lofasz = "picsa"
+    print(length(lofasz))
+    print(length(kecske))
+    a = "h"
+    print(wovel(a))
+
 
 if __name__ == "__main__":
     main()
